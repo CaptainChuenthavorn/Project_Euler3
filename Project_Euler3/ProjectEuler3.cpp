b9ea20c8ea9330d11657908762e0823e1a720656
mymint01 <captainchuenthavorn@gmail.com>
2020-08-15 16:31:55 +0700

    update comment

--- Project_Euler3/ProjectEuler3.cpp
+++ Project_Euler3/ProjectEuler3.cpp
@@ -22,3 +22,61 @@ int main()
 	printf("The largest Prime number factor is %d", prime);
 	return 0;
 }
+/*#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int input,i, a, flag = 0, prime = 0;
+int checkPrime(int N,int i);
+int main()
+{
+	int  k, n,keep;
+	for (i = 2;i < 600851475143;i++) {
+		if (600851475143 % i == 0) {
+			for (a = 2;a <= i/2 ;a++) {
+				if (i % a == 0 || i ==35) {
+					flag = 1;
+					break;
+				}
+				//checkPrime(flag, i);
+				if (flag == 0) {
+					prime = i;
+				}
+			}
+			/*for (k = 2;k < keep;k++) {
+				if (keep % k != 0 )
+				{f
+					prime = keep;
+				}
+			}*/
+			/*if (flag == 0) {
+				prime = i;
+				printf("\nPrime i : %d", prime);
+			}
+			else {
+				printf("NotPrime : %d", i);
+			}*/
+		/*}
+	}
+	printf("\nPrime = %d", prime);
+	return 0;
+}
+int checkPrime(int N, int i) {
+	if (N == 0) {
+		prime = i;
+	}
+	/*for (a = 2;a < (N / 2);a++) {
+		if (N % a == 0)
+			flag = 1;
+		break;
+	}*/
+	/*return prime;
+}*/
+
+//int checkPrime(int input);
+/*int checkprimeCon() {
+
+	scanf("%d", &input);
+	checkPrime(input);
+	if (flag == 0)	printf("Prime");
+	else if (flag == 1)	printf("Not Prime");
+	return 0;
+}*/
