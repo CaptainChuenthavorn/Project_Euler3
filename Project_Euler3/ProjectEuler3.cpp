f9b525d21e9120bb4f0a3656ae3dfd59236c75b0
mymint01 <captainchuenthavorn@gmail.com>
2020-08-15 11:19:10 +0700

    nearly process

--- Project_Euler3/ProjectEuler3.cpp
+++ Project_Euler3/ProjectEuler3.cpp
@@ -1,45 +1,47 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int input,i, a, flag = 0;
-int checkPrime(int N);
+int input,i, a, flag = 0, prime = 0;
+int checkPrime(int N,int i);
 int main()
 {
-	int  k, n,prime=0,keep;
+	int  k, n,keep;
 	for (i = 2;i < 13195;i++) {
 		if (13195 % i == 0) {
-			for (a = 2;a < i/2;a++) {
-				if (i % a == 0)
+			for (a = 2;a <= i/2;a++) {
+				if (i % a == 0) {
 					flag = 1;
-				break;
+					break;
+				}
+				checkPrime(flag, i);	
 			}
-			checkPrime(i);
 			/*for (k = 2;k < keep;k++) {
 				if (keep % k != 0 )
 				{f
 					prime = keep;
 				}
 			}*/
-			if (flag == 0) {
+			/*if (flag == 0) {
 				prime = i;
 				printf("\nPrime i : %d", prime);
 			}
 			else {
 				printf("NotPrime : %d", i);
-			}
+			}*/
 		}
-
 	}
-	printf("\n%d",prime);
+	printf("\nPrime = %d",prime);
 	return 0;
 }
-int checkPrime(int N) {
-
+int checkPrime(int N,int i) {
+	if (N == 0) {
+		prime = i;
+	}	
 	/*for (a = 2;a < (N / 2);a++) {
 		if (N % a == 0)
 			flag = 1;
 		break;
 	}*/
-	return fi;
+	return prime;
 }
 
 //int checkPrime(int input);
