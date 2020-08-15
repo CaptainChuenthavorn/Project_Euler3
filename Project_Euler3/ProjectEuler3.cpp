a99af78a46a35363f11937036b4868ce7aa5184a
mymint01 <captainchuenthavorn@gmail.com>
2020-08-15 12:14:06 +0700

    i think this should  work

--- Project_Euler3/ProjectEuler3.cpp
+++ Project_Euler3/ProjectEuler3.cpp
@@ -3,8 +3,8 @@
 int main()
 {
 	int i, a, flag = 0, prime = 0;
-	for (i = 2;i < 600851475143 / 2;i++) {
-		if (600851475143 % i == 0) {
+	for (i = 2;i < 13195 / 2;i++) {
+		if (13195 % i == 0) {
 			for (a = 2;a <= i / 2;a++) {
 				if (i % a == 0) {
 					flag = 1;
