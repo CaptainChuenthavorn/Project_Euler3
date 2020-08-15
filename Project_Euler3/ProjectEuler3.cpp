ddf62119375069c423afb7e668a18db4621e0cf8
mymint01 <captainchuenthavorn@gmail.com>
2020-08-15 16:29:52 +0700

    lastest finished

--- Project_Euler3/ProjectEuler3.cpp
+++ Project_Euler3/ProjectEuler3.cpp
@@ -2,20 +2,23 @@
 #include<stdio.h>
 int main()
 {
-	int i, a, flag = 0, prime = 0;
-	for (i = 2;i < 13195 / 2;i++) {
-		if (13195 % i == 0) {
+	int i, a, flag = 0, prime = 0, input;
+	printf("Enter number : ");
+	scanf("%d",&input);
+	for (i = 2;i < input / 2;i++) {
+		if (input% i == 0) {
 			for (a = 2;a <= i / 2;a++) {
 				if (i % a == 0) {
 					flag = 1;
 					break;
 				}
-				if (flag == 0) {
-					prime = i;
-				}
+				
+			}
+			if (flag == 0) {
+				prime = i;
 			}
 		}
 	}
-	printf("\nPrime = %d", prime);
+	printf("The largest Prime number factor is %d", prime);
 	return 0;
 }
